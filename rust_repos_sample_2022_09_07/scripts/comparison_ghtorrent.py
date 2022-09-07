12ac90a805698c25072449561d35d43dc9eb814f
William Schueller <william.schueller@gmail.com>
2022-09-08 01:44:34 +0200

    update

--- rust_repos_sample_2022_09_07/scripts/comparison_ghtorrent.py
+++ rust_repos_sample_2022_09_07/scripts/comparison_ghtorrent.py
@@ -7,48 +7,51 @@ import psycopg2
 
 import os
 
+db_name = 'rust_repos_sample'
+port = 5432
+
 # Where to output the files
 output_folder = os.path.dirname(os.path.dirname(__file__))
 
 # Where intermediary data is stored when recollecting data for rebuilding the database
-data_folder = os.path.join(output_folder,'data_folder')
+data_folder = output_folder+'_data_folder'
 
 # Where cloned repositories are stored (when recollecting data for rebuilding the database)
-clones_folder = data_folder
+clones_folder = os.path.join(os.path.dirname(output_folder),db_name.split('_repos')[0]+'_repos','cloned_repos')
 
 
 ### DB connection infos
 # Main DB connection info; where the data is collected/reconstructed from a dump
 db_conninfo = dict( host = 'localhost',
-					port = 5432,
+					port = port,
 					db_user = 'postgres',
 					db_type = 'postgres',
-					db_name = 'rust_repos_sample',
+					db_name = db_name,
 					data_folder = data_folder)
 
 # PG destination DB connection info; where the data is exported before processing (anonymization + cleaning)
 db_dest_pg_conninfo = dict( host = 'localhost',
-					port = 5432,
+					port = port,
 					db_user = 'postgres',
 					db_type = 'postgres',
-					db_name = 'rust_repos_sample_export',
+					db_name = db_name+'_export',
 					data_folder = data_folder)
 
 # SQLite destination DB connection info; where the data is exported before processing (anonymization + cleaning)
 db_dest_sqlite_conninfo = dict(
 					db_type = 'sqlite',
-					db_name = 'rust_repos_sample_export',
+					db_name = db_name+'_export',
 					data_folder = data_folder)
 
 # GHTorrent DB connection info (PostgreSQL)
 db_ght_conninfo = dict( host = 'localhost',
-					port = 5432,
+					port = port,
 					user = 'postgres',
 					database = 'ghtorrent')
 
 # crates.io DB connection info (PostgreSQL)
 db_crates_conninfo = dict( host = 'localhost',
-					port = 5432,
+					port = port,
 					user = 'postgres',
 					database = 'crates_db')
 
